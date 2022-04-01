428dd1051106c5f09489114907ab8fdabf46e85d
rosanna826 <89451716+rosanna826@users.noreply.github.com>
2022-04-01 13:54:44 +1000

    First Commit

---
+++ Terrain 2/Assets/Scripts/DoryMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoryMovement : MonoBehaviour
+{
+    public GameObject human;
+    public float value;
+    public Vector3 sizeChange;
+
+    public void MoveLeft()
+    {
+        value = value - 1f;
+        human.transform.position = new Vector3 (value, 0, 0);
+    }
+
+    public void MoveRight()
+    {
+        value = value + 1f;
+        human.transform.position = new Vector3 (value, 0, 0);
+    }
+
+    public void MoveUp()
+    {
+        value = value + 1f;
+        human.transform.position = new Vector3 (0, value, 0);
+    }
+
+    public void MoveDown()
+    {
+        value = value - 1f;
+        human.transform.position = new Vector3 (0, value, 0);
+
+    }
+
+    public void RotateLeft()
+       {
+           human.transform.Rotate(20f, 20f, 0f);
+
+       }
+    
+    public void GrowUp()
+       {
+           human.transform.localScale = human.transform.localScale + sizeChange;
+       }
+
+    public void GrowDown()
+       {
+           human.transform.localScale = human.transform.localScale - sizeChange;
+       }
+
+
+    public void Reset()
+       {
+            human.transform.position = new Vector3(0,0,0);
+            human.transform.rotation = Quaternion.Euler(new Vector3(0,90,0));
+            human.transform.localScale = new Vector3(1,1,1);
+
+       }
+
+}
